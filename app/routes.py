e629c67fd172233cbc934d399162d577ea4eb06d
f4lco0n <pawel_sokol@o2.pl>
2018-11-23 13:25:09 +0100

    implementacja rejestracji BookBlog-5 done

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,11 @@
 from flask import render_template, flash, redirect, url_for
 from app import app
-from app.forms import LoginForm
-from flask_login import current_user, login_user
+from app.models import User
+from app.forms import LoginForm,RegistrationForm
+from flask_login import current_user, login_user,logout_user
 from flask import request, make_response
 from app import db
-from app.models import User
+
 
 @app.route('/')
 @app.route('/index')
@@ -14,6 +15,7 @@ def index():
 @app.route('/show_books', methods=['GET', 'POST'])
 def show_books():
     return render_template('books.html', title='Books')
+
 @app.route('/login', methods=['GET','POST'])
 def login():
 	if current_user.is_authenticated:
@@ -34,4 +36,26 @@ def login():
 			next_page = url_for('index')
 			return resp
 		return redirect(next_page)
-	return render_template('login.html', title='Sign In', form=form)   
+	return render_template('login.html', title='Sign In', form=form)
+
+@app.route('/logout')
+def logout():
+	logout_user()
+	resp = make_response(redirect('/index'))
+	resp.delete_cookie('user')
+	flash("wylogowano")
+	return resp  
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+	if current_user.is_authenticated:
+		return redirect(url_for('index'))
+	form = RegistrationForm()
+	if form.validate_on_submit():
+		user = User(username=form.username.data, email=form.email.data)
+		user.set_password(form.password.data)
+		db.session.add(user)
+		db.session.commit()
+		flash('Registered')
+		return redirect(url_for('index'))
+	return render_template('register.html', title='Register', form=form) 
