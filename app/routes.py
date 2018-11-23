1841064537aaf3791e194e675c2c7dd2a47417bd
f4lco0n <pawel_sokol@o2.pl>
2018-11-23 11:56:11 +0100

    Utworzenie nawigacji po stronie BookBlog-10

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,17 @@
 from flask import render_template, flash, redirect, url_for
 from app import app
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
 def index():
 	return render_template('index.html', title='Home')
+
+@app.route('/show_books', methods=['GET', 'POST'])
+def show_books():
+    return render_template('books.html', title='Books')
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+	form = LoginForm()
+	return render_template('login.html', title='Sign In', form=form)
